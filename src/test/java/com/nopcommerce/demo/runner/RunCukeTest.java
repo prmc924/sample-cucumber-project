e1bdd20f8dea5572e4f822b728a0908a897ce079
prmc924 <prcm924@gmail.com>
2024-12-14 16:45:04 +0000

    Today's class update

--- src/test/java/com/nopcommerce/demo/runner/RunCukeTest.java
+++ src/test/java/com/nopcommerce/demo/runner/RunCukeTest.java
@@ -1,8 +1,19 @@
 package com.nopcommerce.demo.runner;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
 /**
  * Created by Jay Vaghani
  */
-public class RunCukeTest {
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/nopcommerce/demo/steps",
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "json:target/cucumber-reports/cucumber.json"},
+        tags = "@smoke or @sanity"
+)
+public class RunCukeTest extends AbstractTestNGCucumberTests {
 
 }
